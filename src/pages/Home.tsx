4ef9799d48ea1bcd14aa05f4836921b941c4a90d
Enzo Avagliano <eavagliano6@gmail.com>
2025-05-21 00:11:21 +0200

    Added scss vars + inspi links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 const Home = () => {
+
+  // INSPI
+  // https://www.behance.net/gallery/217595311/Developer-Portfolio-Website
+  // https://www.behance.net/gallery/173956831/Portfolio?tracking_source=search_projects|developer+portfolio&l=3
+  // https://www.behance.net/gallery/132027861/Portfolio-Concept?tracking_source=search_projects|developer+portfolio&l=27
+  // https://dribbble.com/shots/24619466-Portfolio-Website?utm_source=Clipboard_Shot&utm_campaign=ferdousmikdad&utm_content=Portfolio%20Website&utm_medium=Social_Share
+
+
   return (
     <div>
-
+      <h1 className={"h1"}>This is a text heading 1</h1>
     </div>
   );
 };
